c601bfd9a7406d5f921b5c79ef95b96f122226e9
Amarildo Junior <amarildo.junior@fabricads.com.br>
2023-01-29 00:42:11 -0300

    Correção bug de saldo, após execução do stress-test

--- src/test/stress-test/k6-script-realizar-transacao.js
+++ src/test/stress-test/k6-script-realizar-transacao.js
@@ -1,17 +1,20 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
     stages: [
         { duration: '30s', target: 20 },
-        { duration: '1m30s', target: 10 },
+        { duration: '10s', target: 10 },
         { duration: '20s', target: 0 },
     ],
 };
 
 export default function () {
 
-    let data = { numeroCartao: '6549873025634501', senha: '1234', valor: 0.02 };
+    const randomCentavos = randomString(2, '123456789');
+
+    let data = { numeroCartao: '6549873025634501', senha: '1234', valor: '0.' + randomCentavos };
 
     const res = http.post('http://localhost:8080/transacoes', JSON.stringify(data), {
         headers: { 'Content-Type': 'application/json' },
